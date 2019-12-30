1faca110e2ab31dc52a7857a214674b4d431eb9f
Timophey Mironchuk <snakeinacartonbox@gmail.com>
2019-12-30 21:23:59 +0300

    fixed task three

--- MyUniquePtr.cpp
+++ MyUniquePtr.cpp
@@ -11,10 +11,21 @@ MyUniquePtr<T>::MyUniquePtr(T* pointer)
   this->pointer = pointer;
 }
 
-template<typename T>
+template <typename T>
 MyUniquePtr<T>& MyUniquePtr<T>::operator=(MyUniquePtr&& a)
 {
   *pointer = std::move(*a.getPonter());
   return *this;
 }
 
+template <typename T>
+MyUniquePtr<T>::MyUniquePtr(const MyUniquePtr&& a)
+{
+  *pointer = std::move(*a.getPonter());
+}
+
+template <typename T>
+MyUniquePtr<T>::~MyUniquePtr()
+{
+  delete pointer;
+}
